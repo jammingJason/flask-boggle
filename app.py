b8d6d523a7345aa44c7db89685981b82e1885d76
Van Jason Crews <vanjasoncrews@gmail.com>
2022-09-23 15:27:13 -0400

    added code

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 
+from crypt import methods
 from boggle import Boggle
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from random import randint, choice, sample
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'something'
-
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
@@ -14,11 +14,13 @@ boggle_game = Boggle()
 
 @app.route('/')
 def index():
+    """Go to the homepage"""
     return render_template('index.html')
 
 
 @app.route('/start-game')
 def start_game():
+    """Start a new game"""
     new_board = boggle_game.make_board()
     session['board'] = new_board
     return render_template('start-game.html', new_board=new_board)
@@ -26,6 +28,13 @@ def start_game():
 
 @app.route('/check-word')
 def check_word():
+    """Check to see if the word is valid and on the board"""
     str_word = request.args['word']
     check_status = boggle_game.check_valid_word(session['board'], str_word)
-    return render_template('start-game.html', check_status=check_status)
+    return check_status
+
+
+@app.route('/set-highscore')
+def set_highscore():
+    session['highscore'] = request.args['score']
+    return session['highscore']
--- static/app.js
+++ static/app.js
@@ -1,5 +1,68 @@
-// btnGuess = document.querySelector('#btnGuess');
-// btnGuess.addEventListener('click', function (evt) {
-//   evt.preventDefault();
-//   alert();
-// });
+$('#btnGuess').on('click', function (evt) {
+  evt.preventDefault();
+  const strWord = $('#word');
+  //   alert(strWord.val());
+  checkWord(strWord.val());
+});
+
+$('#btnStart').on('click', function () {
+  document.location = '/start-game';
+});
+
+async function checkWord(word) {
+  //   alert(word);
+  let wordCheck = await axios.get('/check-word', { params: { word: word } });
+  //   console.log(wordCheck.data);
+  if (wordCheck.data === 'no') {
+    $('#lblStatus').html('That is not a word!');
+  } else if (wordCheck.data === 'ok') {
+    $('#lblStatus').html('Great!');
+    calculateScore(word);
+  } else if (wordCheck.data === 'not-word') {
+    $('#lblStatus').html('That is not a word!');
+  } else if (wordCheck.data === 'not-on-board') {
+    $('#lblStatus').html('That word is not on the board!');
+  }
+  $('#word').val('');
+}
+
+score = 0;
+function calculateScore(word) {
+  for (x = 0; x < word.length; x++) {
+    score = score + 5;
+  }
+  $('#lblScore').html('Score : ' + score);
+  //   alert(score);
+}
+
+time = 30;
+function timer() {
+  time = time - 1;
+  if (time > 0) {
+    $('#lblTimer').html(time + ' seconds left');
+  } else {
+    $('#lblTimer').html('Game Over!');
+    $('#btnGuess').prop('disabled', true);
+    $('#btnStartOver').prop('hidden', false);
+    setHighscore(score);
+    return false;
+  }
+  setTimeout(timer, 1000);
+}
+timer();
+
+$('#btnStartOver').on('click', function (evt) {
+  document.location = '/';
+});
+
+async function setHighscore(score) {
+  let setHighscore = await axios.get('/set-highscore', {
+    params: { score: score },
+  });
+  getHighscore();
+  console.log(setHighscore.data);
+}
+
+function getHighscore() {
+  $('#lblHighscore').html('Highscore : ' + session['highscore']);
+}
